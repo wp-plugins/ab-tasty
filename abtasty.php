859c0033ee3f2cb9ca5295bcf90eabae7ffaef91
remiaubert <remiaubert@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-28 15:18:30 +0000

    Tag asynchrone
    git-svn-id: https://plugins.svn.wordpress.org/ab-tasty/trunk@847005 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- abtasty.php
+++ abtasty.php
@@ -2,8 +2,8 @@
 /*
  * Plugin Name: ABTasty
  * Plugin URI: http://www.abtasty.com
- * Description: ABTasty wordpress Plugin
- * Version: 1.0
+ * Description: AB Tasty wordpress Plugin
+ * Version: 1.4
  * Author: ABTasty
  * Author URI: http://www.abtasty.com
  * License: none
@@ -34,10 +34,30 @@ if (is_admin())  {
     }
 }
 
-add_action('wp_footer','abtasty', 99999);
+add_action('wp_head','abtasty');
 function abtasty()
 {
     echo "<!-- http://www.abtasty.com -->\n";
-    echo '<script src="//d1447tq2m68ekg.cloudfront.net/' . get_option('abtasty_js_clientid') . '.js" title="AB Tasty : http://www.abtasty.com"></script>';
+    echo '
+	
+	
+	<script>
+    (function(){
+      var ABTastyAccID = "' . get_option('abtasty_js_clientid') . '";
+      var ABTastyTimeout = 2000;
+      var ABTastyIP = "";
+      if("undefined"!==typeof sessionStorage&&"undefined"!==typeof sessionStorage.ABTastyFrameworkCode&&0!=sessionStorage.ABTastyPagesViewed%5)sessionStorage.ABTastyPagesViewed++,"undefined"===typeof sessionStorage.ABTastyDontRun&&eval(sessionStorage.ABTastyFrameworkCode);else{if("undefined"!==typeof sessionStorage&&(void 0==sessionStorage.ABTastyPagesViewed?sessionStorage.ABTastyPagesViewed=0:sessionStorage.ABTastyPagesViewed++,"undefined"!==typeof sessionStorage.ABTastyDontRun))return;var b=(new Date).getTime(),c=document.getElementsByTagName("head")[0],a=document.createElement("script");a.setAttribute("type","text/javascript");a.setAttribute("async","true");a.setAttribute("src","//d1447tq2m68ekg.cloudfront.net/"+ABTastyAccID+".js");/MSIE [678]/.test(window.navigator.userAgent)||(window.stopABTastyAutorun=!0,a.onload=function(){var a=(new Date).getTime()-b;"undefined"!==typeof ABTastyFrameworkCode&&"undefined"!==typeof sessionStorage&&(sessionStorage.ABTastyFrameworkCode=ABTastyFrameworkCode);a>ABTastyTimeout?"undefined"!==typeof sessionStorage&&(sessionStorage.ABTastyDontRun="true"):"undefined"!==typeof ABTastyFrameworkCode&&eval(ABTastyFrameworkCode)});c.appendChild(a)}
+    })();
+  </script>
+  
+  
+  ';
+	
+	
+	
+	
+	
+	
+	
 }
 ?>

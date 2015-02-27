1ac8c62c4a328dbf173056c18e7bbcfaac35e0a2
remiaubert <remiaubert@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-27 15:35:19 +0000

    versionning
    git-svn-id: https://plugins.svn.wordpress.org/ab-tasty/trunk@1101304 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- abtasty.php
+++ abtasty.php
@@ -3,7 +3,7 @@
  * Plugin Name: ABTasty
  * Plugin URI: http://www.abtasty.com
  * Description: AB Tasty wordpress Plugin
- * Version: 2.1
+ * Version: 2.2
  * Author: ABTasty
  * Author URI: http://www.abtasty.com
  * License: none
